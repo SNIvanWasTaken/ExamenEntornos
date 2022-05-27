f30e5939e7fc636e9a768968de418e2801894423
alumno <snivandev@gmail.com>
2022-05-27 18:28:26 +0200

    Commit de documentación

--- ExamendeEntornos/Class1.cs
+++ ExamendeEntornos/Class1.cs
@@ -1,46 +1,30 @@
 ﻿using System;
 
-
-//namespace EDexamenT6a8
-
-
-/*Clase que representa la gestíon de las notas para una asignatura.
- * Se puede(métodos): Introducir la asignatura
- *                    Registrar las notas de prácticas y examen para cada evaluación.
- *                    Calcular las notas por cada trimestre.
- *                    Calcular la nota final de curso.
- * Estos comentarios se deben eliminar y usar comentarios de documentación en su lugar.
- */
-
-
 class Asignatura
 {
-    public string CodigodelaAsignatura;
-    public string NombredelaAsignatura;
-    public double NotadelPrimertrimestre;
-    public double NotadelSegungoTrimestre;
-    public double NotadelTercerTrimestre;
-    public double NotaFinaldelCurso;
+    public string codigodelaAsignatura, nombredelaAsignatura; 
+    public double notadelPrimertrimestre, notadelSegungoTrimestre, notadelSegungoTrimestre, notadelTercerTrimestre, notaFinaldelCurso;
+    //Ya que todas estas variables son del mismo tipo y son asignadas más tarde en el constructor, las ponemos todas en la misma linea para que sea más
+    //lejble y menos líneas de código.
 
+    ///<summary>En este constructor se asignan los atributos los valores que le pasemos al crear el objeto.</summary>
     public Asignatura(string CodigodelaAsignatura, string NombredelaAsignatura, double NotadelPrimertrimestre, double NotadelSegungoTrimestre, double NotadelTercerTrimestre, double NotaFinaldelCurso)
     {
-        this.CodigodelaAsignatura = CodigodelaAsignatura; //Codigo de la asignatura.
-        this.NombredelaAsignatura = NombredelaAsignatura; //Nombre de la asignatura.
-                                                          // Nota de los trimestres (1, 2   y 3) y nota final.
-        this.NotadelPrimertrimestre = NotadelPrimertrimestre;
-        this.NotadelSegungoTrimestre = NotadelSegungoTrimestre;
-        this.NotadelTercerTrimestre = NotadelTercerTrimestre;
-        this.NotaFinaldelCurso = NotaFinaldelCurso;
+        codigodelaAsignatura = CodigodelaAsignatura; 
+        nombredelaAsignatura = NombredelaAsignatura;                    
+        notadelPrimertrimestre = NotadelPrimertrimestre;
+        notadelSegungoTrimestre = NotadelSegungoTrimestre; //He cambiado el nombre de las variables para poder quitar el this, asi se ve mucho mas claro.
+        notadelTercerTrimestre = NotadelTercerTrimestre;    //Además, es conveniente que el nombre de las variables siempre empiece en minuscula.
+        notaFinaldelCurso = NotaFinaldelCurso;
     }
     //He puesto el constructor debajo de los atributos, es mucho mas lejible y manejable. Siempre primero properties, despues atributos,
     //despues constructor y métodos.
 
-
+    ///<remarks>Se recomienda solo usar esta funcion cuando haya que introducir una nueva asignatura</remark>
     public void IntroducirMateria(string Cod_de_Asignatura, string Nombre_de_Asignatura)
     {
         CodigodelaAsignatura = Cod_de_Asignatura; NombredelaAsignatura = Nombre_de_Asignatura;
         //he quitado los .this de esta función, ya que se llaman distinto son muy innecesarios.
-
     }
 
     public void IntroducirlasNotas(double notadelExamen1, double notadelExamen2, double notadelExamen3, double notadelasPracticas1, double notadelasPracticas2, double notadelasPracticas3)
@@ -75,41 +59,28 @@ class Asignatura
         }
     }
 
-    public double calculodelaNota1(double notadelExamen1, double notadelasPracticas1)
+    ///<summary>Estas funciones calculan las notas finales de los respectivos trimestres y la media de la nota final</summary>
+    ///<remark>Poner solo numeros mayores que 0, ya que si no, nos calculará la nota erróneamente.</remark>
+    ///<param name="notadelExamen1">Nota del alumno sacada en el primer trimestre.</param>
+    ///<param name="notadelasPracticas1">Nota del alumno sacada en el primer trimestre.</param>
+    ///<returns>La nota final de los trimestres y la media de la nota final del curso.</returns>
+    public double calculodelaNota1(double notaDelExamen1, double notaDeLasPracticas1)
     {
-        NotadelPrimertrimestre = (notadelExamen1 * 0.8) + (notadelasPracticas1) * 0.2; //La nota está comupuesta  por 20 % de la nota de prácticas y 80 % de la nota del examen. 
-        return NotadelPrimertrimestre;
+        return NotadelPrimertrimestre; = (notaDelExamen1 * 0.8) + (notaDeLasPracticas1) * 0.2;
+        //Al ser una funcion tán simple, no hace falta almacenar en una variable, asi que devolvemos directamente el resultado.
+        //También he cambiado las mayusculas de algunas variables para que resulten más claras.
     }
-    public double calculodelaNota2(double notadelExamen2, double notadelasPracticas2)
+    public double calculodelaNota2(double notaDelExamen2, double notaDeLasPracticas2)
     {
-        NotadelSegungoTrimestre = (notadelExamen2 * 0.8) + (notadelasPracticas2) * 0.2;
-        return NotadelSegungoTrimestre;
+        return NotadelSegungoTrimestre; = (notadelExamen2 * 0.8) + (notaDeLasPracticas2) * 0.2;
     }
-    public double calculodelaNota3(double notadelExamen3, double notadelasPracticas3)
+    public double calculodelaNota3(double notaDelExamen3, double notaDeLasPracticas3)
     {
-        NotadelTercerTrimestre = (notadelExamen3 * 0.8) + (notadelasPracticas3) * 0.2;
-        return NotadelTercerTrimestre;
+        return NotadelTercerTrimestre; = (notadDelExamen3 * 0.8) + (notaDeLasPracticas3) * 0.2;
     }
     public double Calculodelamedia()
     {
-        NotaFinaldelCurso = (NotadelPrimertrimestre + NotadelSegungoTrimestre + NotadelTercerTrimestre) / 3; //Nota final del curso
-        return NotaFinaldelCurso;
+        return NotaFinaldelCurso = (NotadelPrimertrimestre + NotadelSegungoTrimestre + NotadelTercerTrimestre) / 3;
     }//He quitado todos los .this aqui tambien porque son innecesarios.
 
-}
-class ejemplodeunaAsignatura
-{
-
-    //TODO
-    /*
-     *   
-    static void main()
-    {
-        * 
-        * 
-        * 
-        asignatura mi_asignatura_ejemplo = new asignatura("0521385", "Entornos  de Desarrollo");
-
-        /
-    */
 }
